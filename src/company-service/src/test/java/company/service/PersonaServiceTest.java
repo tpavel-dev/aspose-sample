413de5b074d4860ec72035ad770b3727d39b0431
x <x@localhost.localdomain>
2019-09-24 18:41:37 +0600

    optimaze import

--- src/company-service/src/test/java/company/service/PersonaServiceTest.java
+++ src/company-service/src/test/java/company/service/PersonaServiceTest.java
@@ -4,24 +4,17 @@ import company.model.Persona;
 import company.repository.PersonaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-//import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
-//@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {PersonService.class, PersonaRepository.class})
-//@WebAppConfiguration
 @Slf4j
 public class PersonaServiceTest {
 
@@ -35,4 +28,8 @@ public class PersonaServiceTest {
         List<Persona> all = personService.getAll();
         assertEquals(2, all.size());
     }
+
+
+
+
 }
