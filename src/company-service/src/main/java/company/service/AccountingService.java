b70804aff894122c43c6abc9364d2f98b3b63f8c
x <x@localhost.localdomain>
2019-09-24 14:00:29 +0600

    clear gradle repository list

--- src/company-service/src/main/java/company/service/AccountingService.java
+++ src/company-service/src/main/java/company/service/AccountingService.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class AccountingService {
     private OrgStruct orgStruct;
 
-    public void  ProcessOrder(Order order) {
+    public void processOrder(Order order) {
         throw new UnsupportedOperationException("No implemented");
     }
 
